2fba9046f6855068e40a02518efadccb9f7cdefa
qian256 <joewalker.ql@gmail.com>
2016-10-14 01:16:14 -0400

    first commit

---
+++ Assets/AndroidWebcamDisplay.cs
@@ -0,0 +1,111 @@
+﻿// Author: Long Qian
+// Email: lqian8@jhu.edu
+
+using UnityEngine;
+using System.Collections;
+
+public class AndroidWebcamDisplay : MonoBehaviour {
+    
+    private WebCamTexture webCameraTexture;
+
+    // save the webcam texture
+    private Material camTextureHolder;
+
+    // shader of implementing fake AR effect
+    public Material shaderMaterial;
+
+    // backup image
+    public Texture backupImage;
+
+    // a null RenderTexture object to hold place in Graphics.Blit function
+    private RenderTexture nullRenderTexture = null;
+
+    // width and height of actual webcam texture
+    private int webcamWidth, webcamHeight;
+
+    // fps counter
+    const float fpsMeasurePeriod = 0.5f;
+    private int m_FpsAccumulator = 0;
+    private float m_FpsNextPeriod = 0;
+    private int m_CurrentFps;
+    private int fontSize = 40;
+
+
+    // Use this for initialization
+    void Start() {
+        m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
+
+        Debug.Log("Screen width = " + Screen.width + ", height = " + Screen.height);
+
+        // camTextureHolder needs to be initialized before using
+        camTextureHolder = new Material(shaderMaterial);
+
+        // Checks how many and which cameras are available on the device
+        for (int cameraIndex = 0; cameraIndex < WebCamTexture.devices.Length; cameraIndex++) {
+            
+#if UNITY_EDITOR
+            // In editor mode, the camera is usually front facing
+            // grab any camera is fine for testing
+            webCameraTexture = new WebCamTexture(cameraIndex, Screen.width, Screen.height);
+#endif
+            // grab the back facing camera on Android device
+            if (!WebCamTexture.devices[cameraIndex].isFrontFacing) {
+                webCameraTexture = new WebCamTexture(cameraIndex, Screen.width, Screen.height);                
+            }
+
+        }
+
+        camTextureHolder.mainTexture = webCameraTexture;
+        webCameraTexture.Play();
+
+        webcamWidth = webCameraTexture.width;
+        webcamHeight = webCameraTexture.height;
+
+        Debug.Log("WebcamTexture width = " + webcamWidth + ", height = " + webcamHeight);
+
+
+    }
+
+
+    public void ShowCamera() {
+        //myCameraTexture.GetComponent<GUITexture>().enabled = true;
+        webCameraTexture.Play();
+    }
+
+    public void HideCamera() {
+
+        // myCameraTexture.GetComponent<GUITexture>().enabled = false;
+        webCameraTexture.Stop();
+    }
+
+    void OnGUI() {
+        GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = fontSize;
+        // A button to demonstrate how to turn the camera on and off, in case you need it
+        if (GUI.Button(new Rect(0, 0, 200, 60), "ON/OFF")) {
+            if (webCameraTexture.isPlaying)
+                this.HideCamera();
+            else
+                this.ShowCamera();
+        }
+        GUI.Label(new Rect(20, Screen.height - 60, 400, 50), webcamWidth + " x " + webcamHeight + "  " + m_CurrentFps + "fps");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ;
+
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        Graphics.Blit(camTextureHolder.mainTexture, nullRenderTexture, shaderMaterial);
+
+        // measure average frames per second
+        m_FpsAccumulator++;
+        if (Time.realtimeSinceStartup > m_FpsNextPeriod) {
+            m_CurrentFps = (int)(m_FpsAccumulator / fpsMeasurePeriod);
+            m_FpsAccumulator = 0;
+            m_FpsNextPeriod += fpsMeasurePeriod;
+        }
+    }
+
+}
---
+++ Assets/FakeARShader.shader
@@ -0,0 +1,43 @@
+﻿Shader "Custom/FakeAR"
+{
+	Properties{
+		_MainTex("", 2D) = "white" {}
+	}
+
+	SubShader{
+
+		Pass{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc" 
+			//we include "UnityCG.cginc" to use the appdata_img struct
+
+			struct v2f {
+				float4 pos : POSITION;
+				half2 uv : TEXCOORD0;
+			};
+
+			//Our Vertex Shader 
+			v2f vert(appdata_img v) {
+				v2f o;
+				o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = MultiplyUV(UNITY_MATRIX_TEXTURE0, v.texcoord.xy);
+				return o;
+			}
+
+			sampler2D _MainTex;
+	
+			fixed4 frag(v2f i) : COLOR {
+				// Get the orginal rendered color
+				fixed4 orgCol = tex2D(_MainTex, abs(i.uv * 2.0 - 1.0));
+				float avg = (orgCol.r + orgCol.g + orgCol.b) / 3.0;
+				fixed4 col = fixed4(avg, avg, avg, 1);
+
+				return orgCol;
+			}
+			ENDCG
+		}
+	}
+	FallBack "Diffuse"
+}
